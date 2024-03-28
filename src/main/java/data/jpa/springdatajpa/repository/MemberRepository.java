671f6566b1a7ea2df8c19f8208c120f1b7d2aa09
dobiduygi <dobidugi@gmail.com>
2024-03-29 00:08:05 +0900

    feat: JPA @Query 사용

--- src/main/java/data/jpa/springdatajpa/repository/MemberRepository.java
+++ src/main/java/data/jpa/springdatajpa/repository/MemberRepository.java
@@ -2,11 +2,15 @@ package data.jpa.springdatajpa.repository;
 
 import data.jpa.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    public List<Member> findByUsernameAndAgeGreaterThan (String username, int age);
+    List<Member> findByUsernameAndAgeGreaterThan (String username, int age);
 
+    @Query("select m from Member m where m.username = :username")
+    List<Member> findUser(@Param("username") String username);
 }
--- src/test/java/data/jpa/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/data/jpa/springdatajpa/repository/MemberRepositoryTest.java
@@ -71,4 +71,15 @@ class MemberRepositoryTest {
         Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
         Assertions.assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10, null);
+        Member m2 = new Member("BBB", 20, null);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA");
+        Assertions.assertThat(result.get(0)).isEqualTo(m1);
+    }
 }
